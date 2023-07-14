017015bf1b6b81916ec04ea2d6fcbe360f1ae3c0
prabhat1001 <prabhatsaraswat24@gmail.com>
2023-07-15 00:01:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import Dashboard from './components/DashBoard/Dashboard'
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
